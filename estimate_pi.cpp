3edbe9ed356765895523fefe5fb3a1451b3501f8
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-03-22 11:02:02 -0700

    Update estimate_pi.cpp
    Added code for multiple inputs, sommented out cus it doesn't really work yet.

--- estimate_pi.cpp
+++ estimate_pi.cpp
@@ -2,13 +2,16 @@
 #include<iostream>
 #include<cmath>
 
+using namespace std;
+
 int main() {
 	int n;
+	//char operation,redo;
 	cin >> n;
 	long double num_point_circle = 0;
 	long double num_point_total = 0;
 
-	for(int i=0;i<n;i++) {
+	for(int i=0; i<n; i++) {
 		long double x = ((long double) rand() / (RAND_MAX));
 		long double y = ((long double) rand() / (RAND_MAX));
 		long double distance = x*x+y*y;
@@ -21,4 +24,10 @@ int main() {
 
 	long double pi = 4 * num_point_circle/num_point_total;
 	cout<<pi<<endl;
+	/*cout<<"enter y or Y to continue:";
+	cin>>redo;
+	cout<<endl<<endl;
+while(redo=='y'||redo=='Y');
+
+return 0;*/
 }
