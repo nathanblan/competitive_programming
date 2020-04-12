48684ecb906443032197f6592fd104385daf2460
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-04-12 10:06:57 -0700

    Update promotionCounting.cpp

--- promotionCounting.cpp
+++ promotionCounting.cpp
@@ -11,9 +11,12 @@ The third line specifies the number of gold participants before and after the co
 The last line specifies the number of platinum participants before and after the contest.
 output three lines, each with a single int
 line 1 = bronze to silver, 2 = silver to gold, and 3 = gold to plat
-promoteToPlat = plat2 -plat1, StuckInGold = gold1 - promoteToPlat, promoteToGold = gold2 - stuckInGold, stuckInSilver = silver1 - promoteToGold, promoteToSilver = silver1 - stuckInSilver,*/
+promoteToPlat = plat2 -plat1, stuckInGold = gold1 - promoteToPlat, promoteToGold = gold2 - stuckInGold, stuckInSilver = silver1 - promoteToGold, promoteToSilver = silver1 - stuckInSilver*/
 
 int main() {
-  int platPromo = plat2 - plat1
-  int
+  int promoteToPlat = plat2 - plat1;
+  int stuckInGold = gold1 - promoteToPlat;
+  int promoteToGold = gold2 - stuckInGold;
+  int stuckInSilver = silver1 - promoteToGold;
+  int promoteToSilver = silver2 - stuckInSilver;
 }
