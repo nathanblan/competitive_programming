fba6e599f395af29ca56ac83bd99783fa8486ae0
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-05-12 01:39:50 -0700

    Update blockingbillboards.cpp
    why wont it take input

--- blockingbillboards.cpp
+++ blockingbillboards.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int leftx, bottomy, rightx, topy, leftx2, bottomy2, rightx2, topy2, leftTruckX, rightTruckX, bottomTruckY, topTruckY;
@@ -12,42 +12,37 @@ int main() {
   cin >> bottomTruckY>> topTruckY;
 
   //create an array as the range of values the bill boards can be in
-  int canvas [2001][2001];
-  for (int k = 0; k < 2001; k++) {
-    for (int l = 0; l < 2001; l++) {
-      canvas[k][l] = 0;
-    }
-  }
-  //create array with values of bill board 1 which is nested in canvas
-  int billBoard1 [0][0];
+  int range [2001][2001];
+  for (int i = 0; i < 2001; i++) {
+    for (int j = 0; j < 2001; j++) {
+      range[i][j] = 0;
+        }
+      }
   for (int i = leftx; i < rightx; i++) {
     for (int j = bottomy; j < topy; j++) {
-      billBoard1[i][j] = 1;
-    }
-  }
-  //create array with values of bill board 2 which is nested in canvas
-  int billBoard2 [0][0];
+      range[i][j] = 1;
+        }
+      }
   for (int i = leftx2; i < rightx2; i++) {
     for (int j = bottomy2; j < topy2; j++) {
-      billBoard2[i][j] = 1;
-    }
-  }
-  //create array with values of truck (which can overlap bill boards, thus replace those values)
-  int truck [0][0];
+      range[i][j] = 1;
+        }
+      }
   for (int i = leftTruckX; i < rightTruckX; i++) {
     for (int j = bottomTruckY; j < topTruckY; j++) {
-      truck[i][j] = 0;
+      range[i][j] = 0;
     }
   }
-  //count total numbers of bill board values
+
   int visible = 0;
-  for (int c = 0; c < 2001; c++) {
-    for (int d = 0; d < 2001; d++) {
-      if (c || d == 1) {
+  for (int i = 0; i < 2001; i++) {
+    for (int j = 0; j < 2001; j++) {
+      if (range[i][j] == 1) {
         visible += 1;
       }
     }
   }
+
   cout<< visible << endl;
   return 0;
 }
