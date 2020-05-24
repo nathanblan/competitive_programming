4dafac3ec9ce6a9767efc0cb537393a41860f9b2
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-05-23 20:43:45 -0700

    i dont even know anymrore

--- where_am_i.cpp
+++ where_am_i.cpp
@@ -5,10 +5,33 @@
 #include<vector>
 #include<queue>
 #include<string>
-
+#include<set>
 using namespace std;
 
-int N, K;
+int N;
+string S;
+
+bool dups(int len)
+{
+  set<string> X;
+  for (int i=0; i<=N-len; i++) {
+    if (X.count(S.substr(i,len)) > 0) return true;
+    X.insert(S.substr(i,len));
+  }
+  return false;
+}
+
+int main(void)
+{
+  ifstream fin ("whereami.in");
+  ofstream fout ("whereami.out");
+  fin >> N >> S;
+  int ans = 1;
+  while (dups(ans)) ans++;
+  fout << ans << "\n";
+  return 0;
+}
+/*int N, K;
 
 int main() {
   ifstream cin("gymnastics.in");
@@ -16,7 +39,7 @@ int main() {
   cin >> K >> N;
   string farms;
   //loop through the sequence N to check if each value of length K is too small. K works if there are no two sequences of len K in the sequence N
-  //using an array to store each permutation, check if that permutation occurs twice 
+  //using an array to store each permutation, check if that permutation occurs twice
   for (k = 1; k < 100; k++) {
     for (i = 0; i < N; i++) {
       farms.substr(i, K);
@@ -29,3 +52,4 @@ int main() {
     }
   }
 }
+*/
