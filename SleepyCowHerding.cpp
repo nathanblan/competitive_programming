b0c6143f1e5c51555b93baaa5acf7de5e82e10b6
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-05-27 07:01:36 -0700

    Update SleepyCowHerding.cpp

--- SleepyCowHerding.cpp
+++ SleepyCowHerding.cpp
@@ -3,9 +3,30 @@
 using namespace std;
 
 int a, b, c;
-//int b = a+1
-//int c = b+1
-//use a map
+int bestTime, worstTime;
+
 int main() {
-  while ()
+  freopen("teleport.in", "r", stdin);
+  freopen("teleport.out", "w", stdout);
+  cin >> a >> b >> c;
+  bool inOrder = false;
+  int shortTime = 1;
+
+    int cows[3];
+    cows[0] = a;
+    cows[1] = b;
+    cows[3] = c;
+    int n = sizeof(cows)/sizeof(cows[0]);
+    sort(cows, cows+n);
+
+    while (inOrder = false) {
+      c = (a + b)/2;
+      if (b == a+1 && c == b+1) {
+        cout << shortTime << endl;
+        inOrder = true;
+      } else {
+        sort(cows, cows+n);
+          shortTime++;
+    }
+  }
 }
