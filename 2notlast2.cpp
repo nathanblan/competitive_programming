d6250c8947b58482b5899794d7606dc0c879db7e
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-06-01 15:59:31 -0700

    Create 2notlast2.cpp

---
+++ 2notlast2.cpp
@@ -0,0 +1,26 @@
+//notlast2 cus the first one didnt work out very well.
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  freopen("notlast.in", "r", stdin);
+  freopen("notlast.out", "w", stdout);
+
+  int cows[7];
+  cows[0] = 0;
+  cows[1] = 0;
+  cows[2] = 0;
+  cows[3] = 0;
+  cows[4] = 0;
+  cows[5] = 0;
+  cows[6] = 0;
+  /*
+  int N = number of entries.
+  create an array of cows that assigns a cow to an index using cases/ifs and set the value to the amount of milk produced.
+  for (i < N) {
+    check what cow it is, then add their milk amount amount += value
+  }
+  */
+}
