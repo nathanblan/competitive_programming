27c721053428f6d8ae33293bfbe6b4b1c1d6442d
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-06-04 15:55:24 -0700

    Update word_processor.cpp

--- word_processor.cpp
+++ word_processor.cpp
@@ -3,48 +3,30 @@
 
 using namespace std;
 
-int N, K;
-//add strings until length of strings > K, then switch to a new line. Omit spaces when adding strings
 int main() {
-  cin >> N >> K;
-  string text[100];
-  string text_length;
-  string return_text;
-  int j = 0;
-  for (int i = 0; i < N; i++) {
-    cin >> text[i];
-  }
-  for (int i = 0; i < N; i++) {
-      return_text = text[i] + " ";
-      //cout << return_text;
-      text_length += text[i];
-      j++;
-      if (text_length.size() > K) {
-        cout << return_text << endl;
-        text_length.clear();
-        i = j;
-      }
-      else {
-        cout << return_text;
-        i = j;
-      }
-  }
+  freopen("notlast.in", "r", stdin);
+  freopen("notlast.out", "w", stdout);
+
+  int N, K;
+  cin N >> K;
+
+
 }
 
-/*  string text;
-  string return_text;
-  string text_length;
-  cin >> N >> K;
-  for (int i = 0; i < N; i++) {
-    cin >> text;
-    return_text += text + " ";
-    text_length += text;
-  }
-  if (text_length.size() > K) {
-    cout << return_text << endl;
-  }
-  else {
-    cout << return_text;
-  }
-  return 0;
+/*with open("word.in","r") as fin:
+	L = list(fin)
+	N,K = map(int,L[0].split())
+	with open("word.out","w") as fout:
+		w = 0 # current length of line
+		for c in L[1].split(): # go through each word
+			if w+len(c) > K: # place on new line
+				fout.write("\n"+c)
+				w = len(c)
+			else: # place on current line
+				if w > 0:
+					fout.write(" ")
+				fout.write(c)
+				w += len(c)
+		fout.write("\n")
+
   */
