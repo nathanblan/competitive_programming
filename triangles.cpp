a36af6f625c9a7f26305bf3c537083405ae8dc92
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-06-09 22:00:07 -0700

    Update triangles.cpp

--- triangles.cpp
+++ triangles.cpp
@@ -1,4 +1,7 @@
 //USACO 2020 February Contest, Bronze, Problem 1. Triangles
+//https://www.analyzemath.com/calculus/Problems/maximize_area_triangle.html
+//https://math.stackexchange.com/questions/1337696/find-the-maximum-possible-area-of-a-certain-right-triangle
+//to find max size, find the hypotenuse of the triangle and divide it by 2, then apply A = 1/2 base * height (base = hyptonese and height = radius)
 
 #include <bits/stdc++.h>
 
@@ -8,5 +11,9 @@ int main() {
   freopen("ride.in", "r", stdin);
   freopen("ride.out", "w", stdout);
 
-  
+
 }
+/*
+for the first line of input "N", cin x and y in an array
+
+*/
