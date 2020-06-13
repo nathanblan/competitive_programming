00b2b33a3931b4be604784c18bb6e604e686ae3d
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-06-13 15:20:55 -0700

    Update triangles.cpp

--- triangles.cpp
+++ triangles.cpp
@@ -4,10 +4,24 @@
 //to find max size, find the hypotenuse of the triangle and divide it by 2, then apply A = 1/2 base * height (base = hyptonese and height = radius)
 
 #include <bits/stdc++.h>
-
+using ll = long long;
 using namespace std;
 
 int main() {
+  ll n;
+  cin >> n;
+  vector<ll> X(n, 0);
+  vector<ll> Y(n,0);
+  for (ll i=0; i<n;i++) {
+    cin X[i] >> Y[i];
+  }
+  for (ll i=0; i<n;i++) {//corner
+    for (ll j=0; j<n; j++) {//same x coordinate as i
+      for (ll k=0; k<n; k++) {//same y coordinate as i
+        
+      }
+    }
+  }
   freopen("ride.in", "r", stdin);
   freopen("ride.out", "w", stdout);
 
