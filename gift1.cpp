c111ff9b5dcf427e283e89e11431fd839b34e6c3
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-06-18 14:58:47 -0700

    Update gift1.cpp

--- gift1.cpp
+++ gift1.cpp
@@ -7,8 +7,23 @@ LANG: C++
 #include <bits/stdc++.h>
 using namespace std;
 
+int NP;
+
 int main() {
   freopen("gift1.in", "r", stdin);
   freopen("gift1.out", "w", stdout);
-  
+  cin NP;
 }
+/*
+cin amount of people
+for amount of people
+  cin ppl as vector
+if amount > 0
+  cin >> person
+  cin >> amount >> recipients
+  cin people
+  amount per person = amount - (amount % recipients) / recipients
+  add amount % ppl to givers account
+  for recipients
+    add amount per person to each person
+*/
