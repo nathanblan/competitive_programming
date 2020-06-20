e7192da1f9e210f4d40c081b016038da5bd9ef4f
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-06-20 14:22:45 -0700

    Update gift1.cpp

--- gift1.cpp
+++ gift1.cpp
@@ -11,41 +11,33 @@ int NP;
 int amount, people;
 string name, gifted, giver;
 map<string,int> names;
+vector<string> vec;
 int main() {
-//  freopen("gift1.in", "r", stdin);
-//  freopen("gift1.out", "w", stdout);
+  freopen("gift1.in", "r", stdin);
+  freopen("gift1.out", "w", stdout);
   cin >> NP; //number of friends
   for (int i=0;i<NP;i++) {// create a map with index of names
     cin >> name;
     names[name] = 0;//set to 0, add with each transaction
+    vec.push_back(name);// set order of names
   }
   //NP groups of people with gift assignments, iterate to complete each transaction
   for (int i=0; i<NP; i++) {
     cin >> giver;
     cin >> amount >> people;
-    int app = (amount - (amount%people)) / people //app = amount per person
-    for (int j=0;j<people;j++) {
-      cin >> gifted;
-      names[gifted] += app;
+    if (amount != 0) {
+      int app = (amount - (amount%people)) / people; //app = amount per person
+      for (int j=0; j<people; j++) {
+        cin >> gifted;
+        names[gifted] += app;//add transaction to the everyone mentioned by giver
+      }
+      names[giver] += ((amount%people) - amount); //transaction for giver
     }
-    names[giver] += ((amount%people) - amount);
+    else {
+      continue;
+    }
   }
-  for (i = 0; i < NP; i++)
-    cout << names[i] + amount[i] << endl;
+  for (int i=0; i<vec.size(); i++) {
+        cout << vec[i] << " " << names.find(vec[i])->second << endl;
+  }
 }
-/*
-cin amount of people NP
-for amount of people
-  cin ppl as vector[i]
-if amount > 0
-  cin >> person
-  cin >> amount >> recipients
-  cin people
-  amount per person = amount - (amount % recipients) / recipients
-  giver - amount add (amount % ppl)
-  for recipients
-    add amount per person to each person
-
-for (i = 0; i < amount of people; i++)
-  cout << name[i] and amount[i] << endl;
-*/
