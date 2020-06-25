755d92704478c94547a2dacf733a077d1256285b
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-06-24 22:47:51 -0700

    started social distancing USACO bronze 2020 Feb

---
+++ socdist1.cpp
@@ -0,0 +1,17 @@
+//socdist1
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> vec;
+
+int main() {
+  freopen("socdist1.in", "r", stdin);
+  freopen("socdist1.out", "w", stdout);
+  int N;
+  int order;
+  cin >> N;
+  for (int i=0; i<N; i++)
+    cin >> vec[order];
+    cout << order << endl;
+
+}
